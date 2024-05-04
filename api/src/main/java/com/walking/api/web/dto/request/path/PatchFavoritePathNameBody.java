40c1fddbb705f3ed24266dd29d068aaa9ae61a55
belljun3395 <195850@jnu.ac.kr>
2024-05-04 23:18:18 +0900

    feat: PatchFavoritePathNameBody 구현

---
+++ api/src/main/java/com/walking/api/web/dto/request/path/PatchFavoritePathNameBody.java
@@ -0,0 +1,21 @@
+package com.walking.api.web.dto.request.path;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PatchFavoritePathNameBody {
+
+	private String name;
+}
