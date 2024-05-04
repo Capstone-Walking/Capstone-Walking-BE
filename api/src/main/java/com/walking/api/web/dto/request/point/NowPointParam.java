18260e648c828a6794e92b72ac8a4bb53aaf2c9d
belljun3395 <195850@jnu.ac.kr>
2024-05-04 21:46:45 +0900

    feat: NowPointParam을 구현하였습니다

---
+++ api/src/main/java/com/walking/api/web/dto/request/point/NowPointParam.java
@@ -0,0 +1,27 @@
+package com.walking.api.web.dto.request.point;
+
+import com.walking.api.web.dto.request.validator.LatParam;
+import com.walking.api.web.dto.request.validator.LngParam;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NowPointParam {
+
+	/* 현재 위도 */
+	@LatParam private double nowLat;
+
+	/* 현재 경도 */
+	@LngParam private double nowLng;
+}
