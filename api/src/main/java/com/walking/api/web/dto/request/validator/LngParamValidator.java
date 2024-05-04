5e045e72258495d6bcbdd7c1abd0314b4788c40b
belljun3395 <195850@jnu.ac.kr>
2024-05-04 21:45:56 +0900

    feat: LngParam과 검증 객체를 구현하였습니다

---
+++ api/src/main/java/com/walking/api/web/dto/request/validator/LngParamValidator.java
@@ -0,0 +1,39 @@
+package com.walking.api.web.dto.request.validator;
+
+import java.util.Objects;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class LngParamValidator implements ConstraintValidator<LngParam, Double> {
+
+	@Override
+	public boolean isValid(Double value, ConstraintValidatorContext context) {
+		if (Objects.isNull(value)) {
+			addConstraintViolation(context, "lng is null");
+			return false;
+		}
+
+		if (value < 0) {
+			addConstraintViolation(context, "lng is less than 0");
+			return false;
+		}
+
+		/* 경도 범위 */
+		if (!((124 < value) && (value < 132))) {
+			addConstraintViolation(context, "lng is out of range");
+			return false;
+		}
+
+		return true;
+	}
+
+	private void addConstraintViolation(ConstraintValidatorContext context, String message) {
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+	}
+
+	@Override
+	public void initialize(LngParam constraintAnnotation) {
+		ConstraintValidator.super.initialize(constraintAnnotation);
+	}
+}
