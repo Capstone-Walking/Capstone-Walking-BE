f9b51afd2616d7cfc74d27e56daefe872f38f289
belljun3395 <195850@jnu.ac.kr>
2024-05-04 22:52:56 +0900

    feat: GET /api/v1/paths/favorite 컨트롤러를 선언하였습니다

--- api/src/main/java/com/walking/api/web/controller/path/PathController.java
+++ api/src/main/java/com/walking/api/web/controller/path/PathController.java
@@ -1,15 +1,20 @@
 package com.walking.api.web.controller.path;
 
 import com.walking.api.security.authentication.token.TokenUserDetails;
+import com.walking.api.web.dto.request.OrderFilter;
 import com.walking.api.web.dto.request.point.FavoritePathBody;
 import com.walking.api.web.dto.request.point.RoutePointParam;
+import com.walking.api.web.dto.response.BrowseFavoriteRouteResponse;
 import com.walking.api.web.dto.response.RouteDetailResponse;
+import com.walking.api.web.dto.response.detail.FavoritePointDetail;
 import com.walking.api.web.dto.response.detail.PointDetail;
 import com.walking.api.web.dto.response.detail.TrafficDetail;
 import com.walking.api.web.support.ApiResponse;
 import com.walking.api.web.support.ApiResponseGenerator;
 import com.walking.api.web.support.MessageCode;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +24,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -48,6 +54,26 @@ public class PathController {
 		return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
 	}
 
+	@GetMapping("/favorite")
+	public ApiResponse<ApiResponse.SuccessBody<BrowseFavoriteRouteResponse>> browseFavoriteRoute(
+			@AuthenticationPrincipal TokenUserDetails userDetails,
+			@RequestParam(required = true, defaultValue = "createdAt") OrderFilter filter,
+			@RequestParam(required = false) Optional<String> name) {
+		// Long memberId = Long.valueOf(userDetails.getUsername());
+		Long memberId = 999L;
+		if (name.isPresent()) {
+			// todo implement: name 기준 검색
+			log.info("Favorite route browse request: name={}", name.get());
+			BrowseFavoriteRouteResponse response = getSearchFavoriteRouteResponse();
+			return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.SUCCESS);
+		}
+
+		// todo implement: filter 기준 정렬
+		log.info("Favorite route browse request: filter={}", filter);
+		BrowseFavoriteRouteResponse response = getFilterFavoriteRouteResponse();
+		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.SUCCESS);
+	}
+
 	private static RouteDetailResponse getSampleRouteDetailResponse() {
 		return RouteDetailResponse.builder()
 				.totalTime(100L)
@@ -74,6 +100,41 @@ public class PathController {
 						List.of(
 								PointDetail.builder().lat(37.123).lng(127.123).build(),
 								PointDetail.builder().lat(37.456).lng(127.456).build()))
+				.build();
+	}
+
+	private static BrowseFavoriteRouteResponse getSearchFavoriteRouteResponse() {
+		return BrowseFavoriteRouteResponse.builder()
+				.favoritePoints(
+						List.of(
+								FavoritePointDetail.builder()
+										.id(1L)
+										.name("search")
+										.startPoint(PointDetail.builder().lat(37.123).lng(127.123).build())
+										.endPoint(PointDetail.builder().lat(37.456).lng(127.456).build())
+										.createdAt(LocalDateTime.of(2021, 1, 1, 0, 0))
+										.build()))
+				.build();
+	}
+
+	private static BrowseFavoriteRouteResponse getFilterFavoriteRouteResponse() {
+		return BrowseFavoriteRouteResponse.builder()
+				.favoritePoints(
+						List.of(
+								FavoritePointDetail.builder()
+										.id(1L)
+										.name("test1")
+										.startPoint(PointDetail.builder().lat(37.123).lng(127.123).build())
+										.endPoint(PointDetail.builder().lat(37.456).lng(127.456).build())
+										.createdAt(LocalDateTime.of(2021, 1, 1, 0, 0))
+										.build(),
+								FavoritePointDetail.builder()
+										.id(2L)
+										.name("test2")
+										.startPoint(PointDetail.builder().lat(37.123).lng(127.123).build())
+										.endPoint(PointDetail.builder().lat(37.456).lng(127.456).build())
+										.createdAt(LocalDateTime.of(2021, 1, 2, 0, 0))
+										.build()))
 				.build();
 	}
 }
