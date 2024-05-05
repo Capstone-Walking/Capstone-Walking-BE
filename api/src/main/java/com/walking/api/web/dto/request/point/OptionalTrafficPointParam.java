d7e183259ca261a1b048b4d7c9d283d75e3ec213
belljun3395 <195850@jnu.ac.kr>
2024-05-05 15:55:36 +0900

    feat: OptionalTrafficPointParam 구현

---
+++ api/src/main/java/com/walking/api/web/dto/request/point/OptionalTrafficPointParam.java
@@ -0,0 +1,30 @@
+package com.walking.api.web.dto.request.point;
+
+import java.util.Objects;
+import java.util.Optional;
+import javax.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OptionalTrafficPointParam {
+
+	@Nullable private TrafficPointParam trafficPointParam;
+
+	public Optional<TrafficPointParam> get() {
+		return Optional.ofNullable(trafficPointParam);
+	}
+
+	public boolean isPresent() {
+		return Objects.isNull(trafficPointParam);
+	}
+}
