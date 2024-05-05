88369933463c7e7fe80b34d3184713b284d3714b
Moon1C <dudtkd665@gmail.com>
2024-05-06 01:49:04 +0900

    feat: PathFavoritesEntity 생성
    <description>
    <issue-number>

---
+++ data/src/main/java/com/walking/data/entity/member/PathFavoritesEntity.java
@@ -0,0 +1,45 @@
+package com.walking.data.entity.member;
+
+import com.walking.data.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.Point;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@SuperBuilder(toBuilder = true)
+@Table(name = "path_favorites")
+@SQLDelete(sql = "UPDATE path_favorites SET deleted=true where id=?")
+public class PathFavoritesEntity extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MemberEntity memberEntity;
+
+    @Column(columnDefinition = "POINT")
+    private Point startPoint;
+
+    @Column(columnDefinition = "POINT")
+    private Point endPoint;
+
+    @Column(columnDefinition = "LINESTRING")
+    private LineString path;
+
+    @Column(nullable = false)
+    private String startAlias;
+
+    @Column(nullable = false)
+    private String endAlias;
+
+
+
+
+}
