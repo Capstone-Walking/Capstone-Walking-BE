4b28179c9c5712684d500f1aafd13a9ab5339dc9
belljun3395 <195850@jnu.ac.kr>
2024-05-07 00:05:47 +0900

    refactor: OrderFilter#ofRequest 주석을 추가하고 null 그리고 empty은 경우의 기본 값을 설정합니다
    <description>
    OrderFilter의 경우 CREATEDAT을 기본 값으로 가질 수 있도록 관련 코드를 추가하였습니다.

--- api/src/main/java/com/walking/api/web/dto/request/OrderFilter.java
+++ api/src/main/java/com/walking/api/web/dto/request/OrderFilter.java
@@ -1,5 +1,6 @@
 package com.walking.api.web.dto.request;
 
+import java.util.Objects;
 import lombok.Getter;
 
 @Getter
@@ -13,7 +14,16 @@ public enum OrderFilter {
 		this.field = field;
 	}
 
+	/**
+	 * 요청에 따라 정렬할 필드를 반환합니다.
+	 *
+	 * @param source 요청에 따라 정렬할 필드
+	 * @return 정렬할 필드, 요청이 없는 경우 생성일자를 반환합니다.
+	 */
 	public static OrderFilter ofRequest(String source) {
+		if (Objects.isNull(source) || source.isEmpty()) {
+			return CREATEDAT;
+		}
 		return OrderFilter.valueOf(source.toUpperCase());
 	}
 }
