00ca67fbd9b8cab1617c7c2d31f7db585f1a76a7
belljun3395 <195850@jnu.ac.kr>
2024-05-07 00:06:45 +0900

    refactor: 요청 관련 lombok 설정을 통일합니다

--- api/src/main/java/com/walking/api/web/dto/request/path/PatchFavoritePathNameBody.java
+++ api/src/main/java/com/walking/api/web/dto/request/path/PatchFavoritePathNameBody.java
@@ -1,20 +1,19 @@
 package com.walking.api.web.dto.request.path;
 
 import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 @EqualsAndHashCode
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class PatchFavoritePathNameBody {
 
