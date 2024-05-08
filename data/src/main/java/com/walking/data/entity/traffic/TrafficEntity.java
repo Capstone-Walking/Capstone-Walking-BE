a4bc5b8ac09a4f4b848558000253ceb94daa6809
belljun3395 <195850@jnu.ac.kr>
2024-05-08 21:36:13 +0900

    refactor: TrafficEntity를 수정합니다
    <description>
    BaseEntity를 상속하지 않고 기본 정보를 엔티티 안에서 구현합니다.
    위치 컬럼의 SRID를 추가하였습니다.

--- data/src/main/java/com/walking/data/entity/traffic/TrafficEntity.java
+++ data/src/main/java/com/walking/data/entity/traffic/TrafficEntity.java
@@ -1,25 +1,30 @@
 package com.walking.data.entity.traffic;
 
-import com.walking.data.entity.BaseEntity;
+import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 import org.locationtech.jts.geom.Point;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
-@SuperBuilder(toBuilder = true)
+@Builder(toBuilder = true)
 @Table(name = "traffic")
 @SQLDelete(sql = "UPDATE traffic SET deleted=true where id=?")
-public class TrafficEntity extends BaseEntity {
+public class TrafficEntity {
 
 	@Column(nullable = false, updatable = false)
 	private String detail;
@@ -27,6 +32,26 @@ public class TrafficEntity extends BaseEntity {
 	@Column(nullable = false)
 	private String name;
 
-	@Column(columnDefinition = "POINT SRID 4326", nullable = false)
+	@Column(nullable = false, columnDefinition = "POINT SRID 4326")
 	private Point point;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	@Column(nullable = false, updatable = false)
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@Column(nullable = false)
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	@Builder.Default
+	@Column(nullable = false)
+	private Boolean deleted = false;
+
+	public void delete() {
+		this.deleted = true;
+	}
 }
