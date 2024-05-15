7b6d47217c341a2e01db74bcbb6b3b9a3a0a20f8
belljun3395 <195850@jnu.ac.kr>
2024-05-15 20:38:39 +0900

    feat: FavoritePathBody의 start, end 이름을 추가합니다

--- api/src/main/java/com/walking/api/web/dto/request/path/FavoritePathBody.java
+++ api/src/main/java/com/walking/api/web/dto/request/path/FavoritePathBody.java
@@ -4,6 +4,7 @@ import com.walking.api.web.dto.request.validator.LatParam;
 import com.walking.api.web.dto.request.validator.LngParam;
 import java.util.Objects;
 import javax.annotation.Nullable;
+import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,11 +24,15 @@ public class FavoritePathBody {
 	/* 즐겨찾기 경로 이름 */
 	@Nullable private String name;
 
+	@NotBlank private String startName;
+
 	/* 시작점 위도 */
 	@LatParam private double startLat;
 
 	/* 시작점 경도 */
 	@LngParam private double startLng;
+
+	@NotBlank private String endName;
 
 	/* 종료점 위도 */
 	@LatParam private double endLat;
