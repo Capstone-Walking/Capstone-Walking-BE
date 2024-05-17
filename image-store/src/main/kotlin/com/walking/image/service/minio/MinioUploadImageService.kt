e6a7793eaa1615fab6af0351f0037d242669da18
belljun3395 <195850@jnu.ac.kr>
2024-05-18 00:21:11 +0900

    feat: MinioUploadImageService 구현

---
+++ image-store/src/main/kotlin/com/walking/image/service/minio/MinioUploadImageService.kt
@@ -0,0 +1,23 @@
+package com.walking.image.service.minio
+
+import com.walking.image.ImageWriteResponse
+import com.walking.image.MinioImageStoreClient
+import com.walking.image.service.UploadImageService
+import com.walking.image.util.ImageArgsGenerator
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+import java.io.File
+
+@Profile("minio")
+@Service
+class MinioUploadImageService(
+    @Value("\${minio.bucket-name}") val bucket: String,
+    private val imageStoreClient: MinioImageStoreClient
+) : UploadImageService {
+    override fun execute(name: String, file: File): ImageWriteResponse {
+        ImageArgsGenerator.putImage(bucket, name, file).let { args ->
+            return imageStoreClient.putObject(args)
+        }
+    }
+}
