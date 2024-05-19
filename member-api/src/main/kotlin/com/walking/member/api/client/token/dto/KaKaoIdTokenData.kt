5c17ecba601630dd11bb400480f1c973ead30d90
belljun3395 <195850@jnu.ac.kr>
2024-05-19 23:44:45 +0900

    fix: KaKaoIdTokenData 파싱이 되지 않던 문제 해결

--- member-api/src/main/kotlin/com/walking/member/api/client/token/dto/KaKaoIdTokenData.kt
+++ member-api/src/main/kotlin/com/walking/member/api/client/token/dto/KaKaoIdTokenData.kt
@@ -1,6 +1,6 @@
 package com.walking.member.api.client.token.dto
 
-data class KaKaoIdTokenData(private val id_token: String) : SocialIdToken {
+data class KaKaoIdTokenData(val id_token: String) : SocialIdToken {
     override fun getToken(): String {
         return id_token
     }
