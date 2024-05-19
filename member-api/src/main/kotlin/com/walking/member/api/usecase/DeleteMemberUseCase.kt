b065270d130e9c2ba45bf39eac4c95234506a6eb
belljun3395 <195850@jnu.ac.kr>
2024-05-20 02:32:14 +0900

    feat: SocialUnlinkClientManager로 소셜 연결 해지 구현

--- member-api/src/main/kotlin/com/walking/member/api/usecase/DeleteMemberUseCase.kt
+++ member-api/src/main/kotlin/com/walking/member/api/usecase/DeleteMemberUseCase.kt
@@ -2,6 +2,7 @@ package com.walking.member.api.usecase
 
 import com.walking.data.entity.member.MemberEntity
 import com.walking.image.service.minio.MinioRemoveImageService
+import com.walking.member.api.client.unlink.SocialUnlinkClientManager
 import com.walking.member.api.dao.MemberDao
 import com.walking.member.api.usecase.dto.response.DeleteMemberUseCaseResponse
 import org.springframework.cache.annotation.CacheEvict
@@ -11,7 +12,8 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class DeleteMemberUseCase(
     private val memberRepository: MemberDao,
-    private val removeImageService: MinioRemoveImageService
+    private val removeImageService: MinioRemoveImageService,
+    private val unlinkClientManager: SocialUnlinkClientManager
 ) {
     @Transactional
     @CacheEvict(key = "#id", cacheManager = "memberApiCacheManager", cacheNames = ["member-profile-url"])
@@ -19,6 +21,10 @@ class DeleteMemberUseCase(
         val member = memberRepository.findById(id) ?: throw IllegalArgumentException("Member not found")
         val deletedMember = withdrawMember(member)
         removeImageService.execute(deletedMember.profile)
+        unlinkClientManager.execute(
+            deletedMember.certificationSubject.name,
+            deletedMember.certificationId
+        )
 
         return DeleteMemberUseCaseResponse(deletedMember.id, deletedMember.updatedAt)
     }
