6c5625bb58576df780b259a7f1fb8051e14ac788
belljun3395 <195850@jnu.ac.kr>
2024-05-20 17:48:47 +0900

    chore: 임시로 모든 방위의 traffic id를 생성합니다

--- scripts/sql/load-traffic.sql
+++ scripts/sql/load-traffic.sql
@@ -1,15 +1,120 @@
-#신호등 기본 데이터 생성
 LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
-IGNORE
-INTO TABLE api.traffic
-FIELDS
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
     TERMINATED BY ','
-LINES
+    LINES
     TERMINATED BY '\n'
-IGNORE 1 ROWS
-(@itstId, @itstNm, @lat, @lng, @regDt)
-SET
-name = @itstNm,
-detail = @itstId,
-point_value = ST_SRID(POINT(@lng, @lat), 4326),
-created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"nt\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"et\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"st\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"wt\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"ne\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"se\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"sw\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
+
+LOAD DATA INFILE '/var/lib/mysql-files/base-traffic-info.csv'
+    IGNORE
+    INTO TABLE api.traffic
+    FIELDS
+    TERMINATED BY ','
+    LINES
+    TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (@itstId, @itstNm, @lat, @lng, @regDt)
+    SET
+        name = @itstNm,
+        detail =concat( concat("{ \"apiSource\" :\"seoul\", \"direction\" : \"nw\", \"id\": ",@itstId), " }"),
+        point_value = ST_SRID(POINT(@lng, @lat), 4326),
+        created_at = STR_TO_DATE(@regDt, '%Y-%m-%dT%H:%i:%s.000+00:00');
