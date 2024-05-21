892e8706499530a05ffda4b1f3dd1490cc3abc4c
nove1080 <skwpqjq@gmail.com>
2024-05-21 13:47:59 +0900

    feat: TrafficRepository findByIds 추가

--- api/src/main/java/com/walking/api/repository/traffic/TrafficRepository.java
+++ api/src/main/java/com/walking/api/repository/traffic/TrafficRepository.java
@@ -1,8 +1,15 @@
 package com.walking.api.repository.traffic;
 
 import com.walking.data.entity.traffic.TrafficEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrafficRepository extends JpaRepository<TrafficEntity, Long> {}
+public interface TrafficRepository extends JpaRepository<TrafficEntity, Long> {
+
+	@Query("SELECT t FROM TrafficEntity t where t.id IN :ids")
+	List<TrafficEntity> findByIds(@Param("ids") List<Long> ids);
+}
