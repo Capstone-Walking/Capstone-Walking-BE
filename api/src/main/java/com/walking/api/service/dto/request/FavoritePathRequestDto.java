9d338207a62aff1b6180e807fb5311c47b3da536
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2024-05-26 15:50:49 +0900

    feat: FavoritePathRequestDto 추가
    <description>
    <issue-number>

---
+++ api/src/main/java/com/walking/api/service/dto/request/FavoritePathRequestDto.java
@@ -0,0 +1,39 @@
+package com.walking.api.service.dto.request;
+
+import com.walking.api.web.dto.request.validator.LatParam;
+import com.walking.api.web.dto.request.validator.LngParam;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+public class FavoritePathRequestDto {
+
+    /* 즐겨찾기 경로 이름 */
+    @Nullable
+    private String name;
+
+    @NotBlank
+    private String startName;
+
+    /* 시작점 위도 */
+    @LatParam
+    private double startLat;
+
+    /* 시작점 경도 */
+    @LngParam
+    private double startLng;
+
+    @NotBlank private String endName;
+
+    /* 종료점 위도 */
+    @LatParam private double endLat;
+
+    /* 종료점 경도 */
+    @LngParam private double endLng;
+
+
+}
