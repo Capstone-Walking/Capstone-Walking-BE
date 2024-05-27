b9efae42517dee6be330a19f0ccfc241b2aa77ca
nove1080 <skwpqjq@gmail.com>
2024-05-27 16:56:43 +0900

    feat: TrafficDetailConverter 리스트 타입 파라미터 추가

--- api/src/main/java/com/walking/api/converter/TrafficDetailConverter.java
+++ api/src/main/java/com/walking/api/converter/TrafficDetailConverter.java
@@ -4,6 +4,8 @@ import com.walking.api.service.dto.PredictedData;
 import com.walking.api.web.dto.response.detail.PointDetail;
 import com.walking.api.web.dto.response.detail.TrafficDetail;
 import com.walking.data.entity.traffic.TrafficEntity;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class TrafficDetailConverter {
 
@@ -31,5 +33,34 @@ public final class TrafficDetailConverter {
 				.isFavorite(false)
 				.viewName(trafficEntity.getName())
 				.build();
+	}
+
+	/**
+	 * PredictedData를 기반으로 TrafficDetail의 List를 생성합니다.
+	 *
+	 * @param predictedData 사이클 정보 와 현재 색상 및 잔여시간을 예측한 데이터 리스트
+	 * @return 예측 값을 바탕으로 만든 TrafficDetail의 List
+	 */
+	public static List<TrafficDetail> execute(List<PredictedData> predictedData) {
+
+		return predictedData.stream()
+				.map(
+						predictedDatum ->
+								TrafficDetail.builder()
+										.id(predictedDatum.getTraffic().getId())
+										.color(predictedDatum.getCurrentColor().toString())
+										.timeLeft(predictedDatum.getCurrentTimeLeft())
+										.point(
+												PointDetail.builder()
+														.lng(predictedDatum.getTraffic().getLng())
+														.lat(predictedDatum.getTraffic().getLat())
+														.build())
+										.redCycle(predictedDatum.getRedCycle())
+										.greenCycle(predictedDatum.getGreenCycle())
+										.detail(TrafficDetailInfoConverter.execute(predictedDatum.getTraffic()))
+										.isFavorite(false)
+										.viewName(predictedDatum.getTraffic().getName())
+										.build())
+				.collect(Collectors.toList());
 	}
 }
