3d8bd1b08e9fc82a35bacb9e861cfcc4604710e5
nove1080 <skwpqjq@gmail.com>
2024-05-27 16:57:59 +0900

    feat: TrafficRepository 조회 쿼리 추가
    <description>
    findByLocation
    findByLocationAndDistance
    findByTrafficWithinBounds
    추가

--- api/src/main/java/com/walking/api/repository/traffic/TrafficRepository.java
+++ api/src/main/java/com/walking/api/repository/traffic/TrafficRepository.java
@@ -12,4 +12,37 @@ public interface TrafficRepository extends JpaRepository<TrafficEntity, Long> {
 
 	@Query("SELECT t FROM TrafficEntity t where t.id IN :ids")
 	List<TrafficEntity> findByIds(@Param("ids") List<Long> ids);
+
+	@Query(
+			value =
+					"SELECT * FROM traffic "
+							+ "WHERE ST_Equals(point_value, "
+							+ "ST_PointFromText(CONCAT('POINT(', :lat, ' ', :lng, ')'), 4326))",
+			nativeQuery = true)
+	List<TrafficEntity> findByLocation(@Param("lat") Double lat, @Param("lng") Double lng);
+
+	@Query(
+			"SELECT t FROM TrafficEntity t "
+					+ "WHERE FUNCTION('ST_Distance_Sphere', t.point, "
+					+ "FUNCTION('ST_PointFromText', CONCAT('POINT(', :lat, ' ', :lng, ')'), 4326)) < :distance")
+	List<TrafficEntity> findByLocationAndDistance(
+			@Param("lat") Float lat, @Param("lng") Float lng, @Param("distance") Integer distance);
+
+	@Query(
+			value =
+					"SELECT * FROM traffic "
+							+ "WHERE ST_Contains("
+							+ "    ST_SRID("
+							+ "        ST_MakeEnvelope("
+							+ "            POINT(:blLng, :blLat), "
+							+ "            POINT(:trLng, :trLat)"
+							+ "        ), 4326"
+							+ "    ), point_value"
+							+ ")",
+			nativeQuery = true)
+	List<TrafficEntity> findTrafficWithinBounds(
+			@Param("blLng") float blLng,
+			@Param("blLat") float blLat,
+			@Param("trLng") float trLng,
+			@Param("trLat") float trLat);
 }
