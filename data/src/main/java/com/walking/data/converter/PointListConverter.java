5601ff5a5b6165f9244e4537a879056977bb3be7
Moon-1C <dudtkd665@gmail.com>
2024-06-01 15:21:30 +0900

    feat: PointListConverter 추가
    의# 제목을 작성하고 반드시 빈 줄 한 줄을 만들어야 함
    <description>
    <issue-number>

--- data/src/main/java/com/walking/data/converter/PointListConverter.java
+++ data/src/main/java/com/walking/data/converter/PointListConverter.java
@@ -1,35 +1,37 @@
 package com.walking.data.converter;
 
-
+import java.util.Arrays;
+import java.util.List;
+import javax.persistence.AttributeConverter;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 
-import javax.persistence.AttributeConverter;
-import java.util.Arrays;
-import java.util.List;
-
 public class PointListConverter implements AttributeConverter<List<Point>, String> {
 
+	private static final String SPLIT_CHAR = " - ";
 
-    private static final String SPLIT_CHAR = " - ";
+	@Override
+	public String convertToDatabaseColumn(List<Point> pointList) {
+		return String.join(SPLIT_CHAR, pointList.stream().map(Point::toString).toArray(String[]::new));
+	}
 
-    @Override
-    public String convertToDatabaseColumn(List<Point> pointList) {
-        return String.join(SPLIT_CHAR, pointList.stream().map(Point::toString).toArray(String[]::new));
-    }
+	@Override
+	public List<Point> convertToEntityAttribute(String string) {
+		GeometryFactory geometryFactory = new GeometryFactory();
+		List<Point> pointList =
+				Arrays.asList(
+						Arrays.stream(string.split(SPLIT_CHAR))
+								.map(s -> s.replace("POINT (", "").replace(")", ""))
+								.map(
+										s -> {
+											String[] split = s.split(" ");
+											return geometryFactory.createPoint(
+													new Coordinate(
+															Double.parseDouble(split[0]), Double.parseDouble(split[1])));
+										})
+								.toArray(Point[]::new));
 
-    @Override
-    public List<Point> convertToEntityAttribute(String string) {
-        GeometryFactory geometryFactory = new GeometryFactory();
-        List<Point> pointList = Arrays.asList(Arrays.stream(string.split(SPLIT_CHAR)).
-                map(s -> s.replace("POINT (", "").replace(")", "")).
-                map(s -> {
-                    String[] split = s.split(" ");
-                    return geometryFactory.createPoint(new Coordinate(Double.parseDouble(split[0]), Double.parseDouble(split[1])));
-                }).toArray(Point[]::new));
-
-        return pointList;
-    }
-
+		return pointList;
+	}
 }
