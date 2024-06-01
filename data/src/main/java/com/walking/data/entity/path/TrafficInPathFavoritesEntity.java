8b09714fc4d00c680cba0e6e5b5e50f757112b05
Moon-1C <dudtkd665@gmail.com>
2024-06-01 15:21:17 +0900

    refactor: TrafficInPathFavoritesEntity의 trafficTypes의 타입 수정
    <description>
    <issue-number>

--- data/src/main/java/com/walking/data/entity/path/TrafficInPathFavoritesEntity.java
+++ data/src/main/java/com/walking/data/entity/path/TrafficInPathFavoritesEntity.java
@@ -3,8 +3,8 @@ package com.walking.data.entity.path;
 import com.walking.data.converter.PointListConverter;
 import com.walking.data.converter.TMapEnumListConverter;
 import com.walking.data.entity.BaseEntity;
-import com.walking.data.entity.member.MemberEntity;
-import com.walking.data.entity.traffic.TrafficEntity;
+import java.util.List;
+import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,9 +12,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 import org.locationtech.jts.geom.Point;
-
-import javax.persistence.*;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,14 +22,13 @@ import java.util.List;
 @SQLDelete(sql = "UPDATE traffic_in_path_favorites SET deleted=true where id=?")
 public class TrafficInPathFavoritesEntity extends BaseEntity {
 
-    @Convert(converter = PointListConverter.class)
-    private List<Point> trafficPoints;
+	@Convert(converter = PointListConverter.class)
+	private List<Point> trafficPoints;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-    private PathFavoritesEntity pathFk;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	private PathFavoritesEntity pathFk;
 
-    @Convert(converter = TMapEnumListConverter.class)
-    private List<TMapTrafficTurnType> trafficTypes;
-
+	@Convert(converter = TMapEnumListConverter.class)
+	private List<TrafficDirection> trafficTypes;
 }
