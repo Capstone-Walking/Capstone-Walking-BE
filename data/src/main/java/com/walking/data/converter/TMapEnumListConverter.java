7b8b71870d20f8c382d16dd27076afd1eecdb7c0
Moon-1C <dudtkd665@gmail.com>
2024-06-01 15:21:40 +0900

    feat: TMapEnumListConverter
    <description>
    <issue-number>

--- data/src/main/java/com/walking/data/converter/TMapEnumListConverter.java
+++ data/src/main/java/com/walking/data/converter/TMapEnumListConverter.java
@@ -1,25 +1,25 @@
 package com.walking.data.converter;
 
-import com.walking.data.entity.path.TMapTrafficTurnType;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
+import com.walking.data.entity.path.TrafficDirection;
 import java.util.Arrays;
 import java.util.List;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 @Converter
-public class TMapEnumListConverter implements AttributeConverter<List<TMapTrafficTurnType>, String> {
-    private static final String SPLIT_CHAR = ", ";
+public class TMapEnumListConverter implements AttributeConverter<List<TrafficDirection>, String> {
+	private static final String SPLIT_CHAR = ", ";
 
-    @Override
-    public String convertToDatabaseColumn(List<TMapTrafficTurnType> stringList) {
-        return String.join(SPLIT_CHAR, stringList.stream().map(Enum::name).toArray(String[]::new));
-    }
+	@Override
+	public String convertToDatabaseColumn(List<TrafficDirection> stringList) {
+		return String.join(SPLIT_CHAR, stringList.stream().map(Enum::name).toArray(String[]::new));
+	}
 
-    @Override
-    public List<TMapTrafficTurnType> convertToEntityAttribute(String string) {
-        return Arrays.asList(Arrays.stream(string.split(SPLIT_CHAR))
-                .map(TMapTrafficTurnType::valueOf).toArray(TMapTrafficTurnType[]::new));
-
-    }
+	@Override
+	public List<TrafficDirection> convertToEntityAttribute(String string) {
+		return Arrays.asList(
+				Arrays.stream(string.split(SPLIT_CHAR))
+						.map(TrafficDirection::valueOf)
+						.toArray(TrafficDirection[]::new));
+	}
 }
