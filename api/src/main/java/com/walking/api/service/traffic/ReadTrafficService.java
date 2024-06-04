6f04b2cf9a2c76e34466f4947452188bd1ef42f4
nove1080 <skwpqjq@gmail.com>
2024-06-04 16:40:48 +0900

    fix: ReadTrafficService @Transactional(readOnly = true) 추가

--- api/src/main/java/com/walking/api/service/traffic/ReadTrafficService.java
+++ api/src/main/java/com/walking/api/service/traffic/ReadTrafficService.java
@@ -5,6 +5,7 @@ import com.walking.data.entity.traffic.TrafficEntity;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,18 +13,22 @@ public class ReadTrafficService {
 
 	private final TrafficRepository trafficRepository;
 
+	@Transactional(readOnly = true)
 	public TrafficEntity executeById(Long trafficId) {
 		return trafficRepository.findById(trafficId).orElseThrow();
 	}
 
+	@Transactional(readOnly = true)
 	public List<TrafficEntity> executeByIds(List<Long> trafficIds) {
 		return trafficRepository.findByIds(trafficIds);
 	}
 
+	@Transactional(readOnly = true)
 	public List<TrafficEntity> executeByLocationAndDistance(Float lat, Float lng, Integer distance) {
 		return trafficRepository.findByLocationAndDistance(lat, lng, distance);
 	}
 
+	@Transactional(readOnly = true)
 	public List<TrafficEntity> executeWithinBounds(
 			Float blLng, Float blLat, Float trLng, Float trLat) {
 		return trafficRepository.findTrafficWithinBounds(blLng, blLat, trLng, trLat);
