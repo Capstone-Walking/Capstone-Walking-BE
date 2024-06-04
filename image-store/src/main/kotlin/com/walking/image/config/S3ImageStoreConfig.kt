6238abeb313876dbbb71a793b1169ac8f129d108
belljun3395 <195850@jnu.ac.kr>
2024-06-04 18:13:31 +0900

    feat: S3 이미지 기능을 구현

---
+++ image-store/src/main/kotlin/com/walking/image/config/S3ImageStoreConfig.kt
@@ -0,0 +1,57 @@
+package com.walking.image.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.walking.image.S3ImageStoreClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.context.event.ContextRefreshedEvent
+
+@Profile("!local")
+@Configuration
+class S3ImageStoreConfig(
+    @Value("\${s3.url}") val url: String,
+    @Value("\${s3.access-key}") val accessKey: String,
+    @Value("\${s3.secret-key}") val secretKey: String,
+    @Value("\${s3.bucket-name}") val bucket: String,
+    @Value("\${s3.region}:ap-northeast-2") val region: String
+) : ApplicationListener<ContextRefreshedEvent> {
+
+    var log: Logger = LoggerFactory.getLogger(S3ImageStoreConfig::class.java)
+
+    private var client: AmazonS3Client? = null
+
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
+        client?.let { client ->
+            if (!client.doesBucketExistV2(bucket)) {
+                client.createBucket(bucket)
+                log.info("Create bucket $bucket")
+            } else {
+                log.info("Bucket $bucket already exists")
+            }
+        }
+    }
+
+    @Bean
+    fun s3ImageStoreClient(): S3ImageStoreClient {
+        AmazonS3Client.builder()
+            .withRegion(region)
+            .withCredentials(
+                AWSStaticCredentialsProvider(
+                    BasicAWSCredentials(
+                        accessKey,
+                        secretKey
+                    )
+                )
+            ).build().let { client ->
+                this.client = client as AmazonS3Client
+                return S3ImageStoreClient(client, region)
+            }
+    }
+}
