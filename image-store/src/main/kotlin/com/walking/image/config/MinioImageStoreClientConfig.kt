038ffbcdb7f33adaeb7a3990e891dbb0ae1713f5
belljun3395 <195850@jnu.ac.kr>
2024-06-04 18:13:56 +0900

    refactor: Minio 클래스 프로필 수정

--- image-store/src/main/kotlin/com/walking/image/config/MinioImageStoreClientConfig.kt
+++ image-store/src/main/kotlin/com/walking/image/config/MinioImageStoreClientConfig.kt
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.context.event.ContextRefreshedEvent
 
-@Profile("minio")
+@Profile("local")
 @Configuration
 class MinioImageStoreClientConfig(
     @Value("\${minio.url}") val url: String,
--- image-store/src/main/kotlin/com/walking/image/service/minio/MinioUploadImageService.kt
+++ image-store/src/main/kotlin/com/walking/image/service/minio/MinioUploadImageService.kt
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import java.io.File
 
-@Profile("minio")
+@Profile("local")
 @Service
 class MinioUploadImageService(
     @Value("\${minio.bucket-name}") val bucket: String,
