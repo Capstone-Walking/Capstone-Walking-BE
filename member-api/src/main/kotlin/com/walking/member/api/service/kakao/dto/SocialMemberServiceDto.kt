d26ddada3d43f991e457f696041c8e93714eb3cf
belljun3395 <195850@jnu.ac.kr>
2024-06-10 13:43:24 +0900

    feat: 기본 이미지 랜덤으로 설정되도록 기능 추가

--- member-api/src/main/kotlin/com/walking/member/api/service/kakao/dto/SocialMemberServiceDto.kt
+++ member-api/src/main/kotlin/com/walking/member/api/service/kakao/dto/SocialMemberServiceDto.kt
@@ -2,7 +2,7 @@ package com.walking.member.api.service.kakao.dto
 
 data class SocialMemberServiceDto(
     val nickName: String,
-    val profile: String,
+    var profile: String,
     val certificationId: String,
     val certificationSubject: String
 )
--- member-api/src/main/kotlin/com/walking/member/api/usecase/PostMemberUseCase.kt
+++ member-api/src/main/kotlin/com/walking/member/api/usecase/PostMemberUseCase.kt
@@ -7,12 +7,20 @@ import com.walking.member.api.usecase.dto.response.PostMemberUseCaseResponse
 import com.walking.member.api.service.kakao.PostKaKaoMemberService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.Random
 
 @Service
 class PostMemberUseCase(
     private val createKaKaoMemberService: PostKaKaoMemberService,
     private val memberRepository: MemberDao
 ) {
+
+    companion object{
+        val RED_PROFILE = "https://d25bhk5us0632l.cloudfront.net/default/profiles/red.png"
+        val GREEN_PROFILE = "https://d25bhk5us0632l.cloudfront.net/default/profiles/green.png"
+        val YELLOW_PROFILE = "https://d25bhk5us0632l.cloudfront.net/default/profiles/yellow.png"
+        val DEFAULT_PROFILES = listOf(RED_PROFILE, GREEN_PROFILE, YELLOW_PROFILE)
+    }
     @Transactional
     fun execute(code: String): PostMemberUseCaseResponse {
         val socialMember = createKaKaoMemberService.execute(code)
@@ -29,6 +37,10 @@ class PostMemberUseCase(
     }
 
     private fun createMemberEntity(socialMember: SocialMemberServiceDto): MemberEntity {
+        Random().nextInt(DEFAULT_PROFILES.size).let { index ->
+            socialMember.profile = DEFAULT_PROFILES[index]
+        }
+
         return MemberEntity(
             socialMember.nickName,
             socialMember.profile,
