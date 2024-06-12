c810c1cca419c095b38560f4444ae236e47f9b46
belljun3395 <195850@jnu.ac.kr>
2024-06-12 17:14:01 +0900

    fix: 컨버팅 안되는 문제 해결

--- api/src/main/java/com/walking/api/web/dto/response/detail/TrafficDetailInfo.java
+++ api/src/main/java/com/walking/api/web/dto/response/detail/TrafficDetailInfo.java
@@ -1,7 +1,6 @@
 package com.walking.api.web.dto.response.detail;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,11 +10,16 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class TrafficDetailInfo {
 	private Long trafficId;
 	private String apiSource;
 	private String direction;
+
+	public TrafficDetailInfo(Long trafficId, String apiSource, String direction) {
+		this.trafficId = trafficId;
+		this.apiSource = apiSource;
+		this.direction = direction;
+	}
 }
