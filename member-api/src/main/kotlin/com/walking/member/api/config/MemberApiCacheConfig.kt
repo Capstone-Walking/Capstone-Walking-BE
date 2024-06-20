9e5e6fcb4970e60d61b1346dd8a44d6415ce548c
belljun3395 <195850@jnu.ac.kr>
2024-06-20 09:29:01 +0900

    feat: ttl 적용을 위한 caffeine 캐시 적용

--- member-api/src/main/kotlin/com/walking/member/api/config/MemberApiCacheConfig.kt
+++ member-api/src/main/kotlin/com/walking/member/api/config/MemberApiCacheConfig.kt
@@ -1,26 +1,32 @@
 package com.walking.member.api.config
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCache
-import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.List
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableCaching
 class MemberApiCacheConfig {
 
+    @Bean
+    fun caffeineConfig(): Caffeine<Any, Any> {
+        return Caffeine.newBuilder()
+            .initialCapacity(16)
+            .maximumSize(100)
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .recordStats()
+    }
+
     @Bean(value = [MemberApiConfig.BEAN_NAME_PREFIX + "CacheManager"])
     fun memberApiCacheManager(): CacheManager {
-        SimpleCacheManager().apply {
-            this.setCaches(
-                List.of(
-                    ConcurrentMapCache("member-profile")
-                )
-            )
-            return this
+        CaffeineCacheManager().let { cacheManager ->
+            cacheManager.setCaffeine(caffeineConfig())
+            cacheManager.cacheNames = listOf("member-profile")
+            return cacheManager
         }
     }
 }
